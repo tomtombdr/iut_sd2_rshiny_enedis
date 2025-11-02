04065b065568995dbe15415807247739c834f287
Evanessentiel <evanlane38@gmail.com>
2025-11-02 09:48:18 +0100

    Update app.R

--- app.R
+++ app.R
@@ -32,13 +32,13 @@ server <- function(input, output) {
   output$Répartition_surface_maison_73 <- renderPlot({
     
     df_maison = df_total[df_total$type_batiment == "maison",]
-    df_maison = df_maison[df_maison$surface_habitable_logement <=500,]
+    df_maison = df_maison[df_maison$surface_habitable_logement <=350,]
     df_maison_73 = df_maison[grepl("^73", df_maison$code_insee_ban), ]
     hist(
       df_maison_73$surface_habitable_logement,
-      breaks = seq(0, 500, length.out = 11),
-      xlim = c(0, 500),
-      ylim = c(0, 5000),
+      breaks = seq(0, 350, length.out = 8),
+      xlim = c(0, 350),
+      ylim = c(0, 2000),
       xlab = "Surface habitable",
       ylab = "Effectifs",
       main = "Répartition des surfaces habitables des maisons du 73",
@@ -49,13 +49,13 @@ server <- function(input, output) {
   output$Répartition_surface_maison_74 <- renderPlot({
     
     df_maison = df_total[df_total$type_batiment == "maison",]
-    df_maison = df_maison[df_maison$surface_habitable_logement <=500,]
+    df_maison = df_maison[df_maison$surface_habitable_logement <=350,]
     df_maison_74 = df_maison[grepl("^74", df_maison$code_insee_ban), ]
     hist(
       df_maison_74$surface_habitable_logement,
-      breaks = seq(0, 500, length.out = 11),
-      xlim = c(0, 500),
-      ylim = c(0, 5000),
+      breaks = seq(0, 350, length.out = 8),
+      xlim = c(0, 350),
+      ylim = c(0, 2000),
       xlab = "Surface habitable",
       ylab = "Effectifs",
       main = "Répartition des surfaces habitables des maisons du 74",
